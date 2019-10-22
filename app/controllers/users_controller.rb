109ab8ec3abd4e740cd658fd1a4b3cbf4662b930
Ben John Bagley <hello@benbagley.dev>
2019-10-22 23:48:31 +0100

    user is now showing the correct albums that apply only to them, user url added to albums

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def show
-    @user = HTTParty
-      .get('https://jsonplaceholder.typicode.com/users/' + params[:id], :headers => {'Content-Type' => 'application/json'})
+    @user = HTTParty.get('https://jsonplaceholder.typicode.com/users/' + params[:id], :headers => {'Content-Type' => 'application/json'})
 
     @albums = HTTParty
       .get('https://jsonplaceholder.typicode.com/albums', :headers => {'Content-Type' => 'application/json'})
+      .select{|a| a['userId'] == @user['id']}
       .paginate(:page => params[:page], :per_page => 6)
   end
 end
