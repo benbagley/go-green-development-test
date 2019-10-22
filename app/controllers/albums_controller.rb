738bf5faa630ce0043fdbdcfefcf2e5b39fa7eac
Ben John Bagley <hello@benbagley.dev>
2019-10-22 20:53:33 +0100

    display album owners name and display first thumbnail

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -3,23 +3,13 @@ require 'will_paginate/array'
 class AlbumsController < ApplicationController
   def index
     @albums = HTTParty.get('https://jsonplaceholder.typicode.com/albums', :headers => {'Content-Type' => 'application/json'}).paginate(:page => params[:page], :per_page => 10)
-    @user = HTTParty.get('https://jsonplaceholder.typicode.com/users', :headers => {'Content-Type' => 'application/json'})
-
-    @albums.each do |album|
-      user = @users.first {|u| u if u.id == album.userId }
-      album.merge!({ name: user.name, email: user.email })  if user.present?
-    end
+    @albumPhotos = HTTParty.get('https://jsonplaceholder.typicode.com/photos?albums/', :headers => {'Content-Type' => 'application/json'})
+    @users = HTTParty.get('https://jsonplaceholder.typicode.com/users', :headers => {'Content-Type' => 'application/json'})
   end
 
   def show
-    @album = HTTParty
-      .get('https://jsonplaceholder.typicode.com/albums/' + params[:id], :headers => {'Content-Type' => 'application/json'})
-
-    @albumPhotos = HTTParty
-      .get('https://jsonplaceholder.typicode.com/photos?album=' + params[:id], :headers => {'Content-Type' => 'application/json'})
-      .paginate(:page => params[:page], :per_page => 10)
-
-    @user = HTTParty
-      .get('https://jsonplaceholder.typicode.com/users/' + params[:id], :headers => {'Content-Type' => 'application/json'})
+    @album = HTTParty.get('https://jsonplaceholder.typicode.com/albums/' + params[:id], :headers => {'Content-Type' => 'application/json'})
+    @albumPhotos = HTTParty.get('https://jsonplaceholder.typicode.com/photos?album=' + params[:id], :headers => {'Content-Type' => 'application/json'}).paginate(:page => params[:page], :per_page => 10)
+    @user = HTTParty.get('https://jsonplaceholder.typicode.com/users/' + params[:id], :headers => {'Content-Type' => 'application/json'})
   end
 end
