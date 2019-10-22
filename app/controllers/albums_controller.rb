420137869652c3b7675190d55a9804523d4a0ee7
Ben John Bagley <hello@benbagley.dev>
2019-10-22 16:51:50 +0100

    tests

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,11 +2,13 @@ require 'will_paginate/array'
 
 class AlbumsController < ApplicationController
   def index
-    @albums = HTTParty
-      .get('https://jsonplaceholder.typicode.com/albums', :headers => {'Content-Type' => 'application/json'})
-      .paginate(:page => params[:page], :per_page => 10)
-    @user = HTTParty
-      .get('https://jsonplaceholder.typicode.com/users', :headers => {'Content-Type' => 'application/json'})
+    @albums = HTTParty.get('https://jsonplaceholder.typicode.com/albums', :headers => {'Content-Type' => 'application/json'}).paginate(:page => params[:page], :per_page => 10)
+    @user = HTTParty.get('https://jsonplaceholder.typicode.com/users', :headers => {'Content-Type' => 'application/json'})
+
+    @albums.each do |album|
+      user = @users.first {|u| u if u.id == album.userId }
+      album.merge!({ name: user.name, email: user.email })  if user.present?
+    end
   end
 
   def show
