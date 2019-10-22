e694ea0f6558a70131b91fccab5555281f22edcc
Ben John Bagley <hello@benbagley.dev>
2019-10-22 21:57:02 +0100

    display correct user information on album show page

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -10,6 +10,6 @@ class AlbumsController < ApplicationController
   def show
     @album = HTTParty.get('https://jsonplaceholder.typicode.com/albums/' + params[:id], :headers => {'Content-Type' => 'application/json'})
     @albumPhotos = HTTParty.get('https://jsonplaceholder.typicode.com/photos?album=' + params[:id], :headers => {'Content-Type' => 'application/json'}).paginate(:page => params[:page], :per_page => 10)
-    @user = HTTParty.get('https://jsonplaceholder.typicode.com/users/' + params[:id], :headers => {'Content-Type' => 'application/json'})
+    @users = HTTParty.get('https://jsonplaceholder.typicode.com/users/', :headers => {'Content-Type' => 'application/json'})
   end
 end
